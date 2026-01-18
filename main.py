2948624edaeee387ecda64a04ba178c99a9d1f11
th31dk <andrew.kxu@gmail.com>
2026-01-18 13:03:32 -0800

    added ATBASH

---
+++ main.py
@@ -0,0 +1,11 @@
+ATBASH = {'A':'Z', 'B':'Y', 'C':'X', 'D':'W', 'E':'V', 'F':'U', 'G':'T', 'H':'S', 'I':'R', 'J':'Q', 'K':'P', 'L':'O', 'M':'N',
+          'N':'M', 'O':'L', 'P':'K', 'Q':'J', 'R':'I', 'S':'H', 'T':'G', 'U':'F', 'V':'E', 'W':'D', 'X':'C', 'Y':'B', 'Z':'A'}
+
+def encode(message):
+    cipher = ''
+    for letter in message:
+        cipher += ATBASH[letter]
+
+    return cipher
+
+print(encode('HELLO'))
