061f8392ab5d28d433d4a41c10fac54eb2167925
th31dk <andrew.kxu@gmail.com>
2026-01-19 22:33:46 -0800

    fully implemented caesar cipher

--- Cipher Encoder.py
+++ Cipher Encoder.py
@@ -2,9 +2,9 @@ message = str(input('What message would you like to encode?')).upper()
 validCipher = False
 
 while validCipher == False:
-    cipherType = str(input("What Cipher would you like to use?(ATBASH or CA)")).upper()
+    cipherType = str(input("What Cipher would you like to use?(ATBASH or CAESAR)")).upper()
     
-    if cipherType == 'ATBASH':
+    if cipherType == "ATBASH":
         validCipher = True
         ATBASH = {'A':'Z', 'B':'Y', 'C':'X', 'D':'W', 'E':'V', 'F':'U', 'G':'T', 'H':'S', 'I':'R', 'J':'Q', 'K':'P', 'L':'O', 'M':'N',
                 'N':'M', 'O':'L', 'P':'K', 'Q':'J', 'R':'I', 'S':'H', 'T':'G', 'U':'F', 'V':'E', 'W':'D', 'X':'C', 'Y':'B', 'Z':'A'}
@@ -17,6 +17,18 @@ while validCipher == False:
                     cipher += ATBASH[letter]
             return cipher
         encodedMessage = ATBASHencode(message)
+
+    elif cipherType == "CAESAR":
+        validCipher = True
+        alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        def CAESARencode(message):
+            shift = int(input("What shift?")) % 26
+            cipherKey = alphabet[shift:] + alphabet[:shift]
+            encode = str.maketrans(alphabet, cipherKey)
+            cipher = message.translate(encode)
+            return cipher
+        encodedMessage = CAESARencode(message)
+
 
     else:
         print('INVALID CIPHER TYPE, PLEASE TRY AGAIN')
