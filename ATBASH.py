3cd6b2e944d4234cca1dc1e8919968aca342fc3e
th31dk <andrew.kxu@gmail.com>
2026-01-19 21:45:02 -0800

    renamed file to ATBASH

--- ATBASH.py
+++ ATBASH.py
@@ -3,7 +3,7 @@ validCipher = False
 
 while validCipher == False:
     cipherType = str(input("What Cipher would you like to use?(Atbash)")).upper()
-
+    
     if cipherType == 'ATBASH':
         validCipher = True
         ATBASH = {'A':'Z', 'B':'Y', 'C':'X', 'D':'W', 'E':'V', 'F':'U', 'G':'T', 'H':'S', 'I':'R', 'J':'Q', 'K':'P', 'L':'O', 'M':'N',
