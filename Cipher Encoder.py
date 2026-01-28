be68a596c334e7b007bfff0b4ab7e07ee7f720b5
th31dk <andrew.kxu@gmail.com>
2026-01-28 12:44:24 -0800

    began organizing the code a bit

--- Cipher Encoder.py
+++ Cipher Encoder.py
@@ -1,3 +1,30 @@
+# Global Variables
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# ATBASH
+ATBASHdictionary = {'A':'Z', 'B':'Y', 'C':'X', 'D':'W', 'E':'V', 'F':'U', 'G':'T', 'H':'S', 'I':'R', 'J':'Q', 'K':'P', 'L':'O', 'M':'N',
+          'N':'M', 'O':'L', 'P':'K', 'Q':'J', 'R':'I', 'S':'H', 'T':'G', 'U':'F', 'V':'E', 'W':'D', 'X':'C', 'Y':'B', 'Z':'A'}
+def ATBASH(message):
+    cipher = ''
+    for characters in message:
+        if characters == ' ':
+            cipher += ' '
+        else:
+            cipher += ATBASHdictionary[characters]
+    return cipher
+
+# CAESAR
+def CAESARencode(message):
+    shift = int(input("What shift?")) % 26
+    cipherKey = alphabet[shift:] + alphabet[:shift]
+    encode = str.maketrans(alphabet, cipherKey)
+    cipher = message.translate(encode)
+    return cipher
+
+# BINARY
+
+# VIGENERE
+
 goodanswer = False
 while goodanswer == False:
     encodeordecode = str(input("Would you like to decode or encode?")).upper()
@@ -11,27 +38,10 @@ while goodanswer == False:
             
             if cipherType == "ATBASH":
                 validCipher = True
-                ATBASH = {'A':'Z', 'B':'Y', 'C':'X', 'D':'W', 'E':'V', 'F':'U', 'G':'T', 'H':'S', 'I':'R', 'J':'Q', 'K':'P', 'L':'O', 'M':'N',
-                        'N':'M', 'O':'L', 'P':'K', 'Q':'J', 'R':'I', 'S':'H', 'T':'G', 'U':'F', 'V':'E', 'W':'D', 'X':'C', 'Y':'B', 'Z':'A'}
-                def ATBASHencode(message):
-                    cipher = ''
-                    for characters in message:
-                        if characters == ' ':
-                            cipher += ' '
-                        else:
-                            cipher += ATBASH[characters]
-                    return cipher
                 encodedMessage = ATBASHencode(message)
 
             elif cipherType == "CAESAR":
                 validCipher = True
-                alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-                def CAESARencode(message):
-                    shift = int(input("What shift?")) % 26
-                    cipherKey = alphabet[shift:] + alphabet[:shift]
-                    encode = str.maketrans(alphabet, cipherKey)
-                    cipher = message.translate(encode)
-                    return cipher
                 encodedMessage = CAESARencode(message)
 
             elif cipherType == "BINARY":
@@ -45,7 +55,6 @@ while goodanswer == False:
 
             elif cipherType == "VIGENERE":
                 validCipher = True
-                alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                 alphabetValues = {'A' : 1, 'B' : 2, 'C' : 3, 'D' : 4, 'E' : 5, 'F' : 6, 'G' : 7, 'H' : 8, 'I' : 9, 'J' : 10, 'K' : 11, 'L' : 12, 'M' : 13,
                                 'N' : 14, 'O' : 15, 'P' : 16, 'Q' : 17, 'R' : 18, 'S' : 19, 'T' : 20, 'U' : 21, 'V' : 22, 'W' : 23,'X' : 24,'Y' : 25, 'Z' : 26}
                 
